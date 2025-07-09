da38c781fe65ed1b666a6472cd663191de371f83
joyce sabwe <joycesabwe@gmail.com>
2025-07-09 23:41:54 +0100

    upload

--- init.sql
+++
@@ -1,7 +0,0 @@
--- init.sql
-
-CREATE USER bright_user WITH PASSWORD 'bright_pass';
-CREATE DATABASE bright_db OWNER bright_user;
-GRANT ALL PRIVILEGES ON DATABASE bright_db TO bright_user;
-
-\echo '✔ init.sql exécuté avec succès'
