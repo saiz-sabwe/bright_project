0a779ec6ffff6f9a915a4d3cae13b58cd1d1ea8a
joyce sabwe <joycesabwe@gmail.com>
2025-07-09 23:28:04 +0100

    upload

--- init.sql
+++ init.sql
@@ -1,9 +1,7 @@
 -- init.sql
--- Supprime l'utilisateur s'il existe déjà (facultatif)
-DROP DATABASE IF EXISTS bright_db;
-DROP USER IF EXISTS bright_user;
 
--- Création de l'utilisateur et de la base
 CREATE USER bright_user WITH PASSWORD 'bright_pass';
 CREATE DATABASE bright_db OWNER bright_user;
 GRANT ALL PRIVILEGES ON DATABASE bright_db TO bright_user;
+
+\echo '✔ init.sql exécuté avec succès'
